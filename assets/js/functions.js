f65aa4b72661cf71436e6bc181663fd1c4a29db7
houterwamal <wouterhamal@hotmail.com>
2016-11-08 10:01:24 +0100

    final update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -36,6 +36,44 @@ $(document).ready(function () {
     
     });
     
+    //mobile-nav
+    $('#nav-bars').on('click', function(){
+        $(this).fadeOut(function(){
+            $('#nav-cross').fadeIn(); 
+            $('#nav-list').fadeIn();
+        });
+    });
+    
+    $('#nav-cross').on('click', function(){
+        $(this).fadeOut(function(){
+            $('#nav-list').fadeOut();
+            $('#nav-bars').fadeIn(); 
+        });
+    });
+    
+    // Add smooth scrolling to all links
+      $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+
+          // Store hash
+          var hash = this.hash;
+            
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+    
     //start fadein
     $("#intro-overlay").hide().fadeIn(2000);
     
